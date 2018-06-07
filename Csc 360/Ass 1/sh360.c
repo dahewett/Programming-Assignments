002805614a9f78bb08c5aeb6bcae2d10c70f780c
dhewett <dah12hewett@gmail.com>
2018-06-06 19:58:50 -0700

    Left to do: parsing and Piping, PEE PEE!

--- Csc 360/Ass 1/sh360.c
+++ Csc 360/Ass 1/sh360.c
@@ -12,21 +12,33 @@
 
 #include <sys/wait.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 #define MAX_INPUT_LINE 80
 #define MAX_NUM_ARGS 7
+#define MAX_PROMPT 10
 
 
-void cmd_loop(char* input, int* tp, char* t);
+void cmd_loop(char* input, char** token, int* tp, char* t);
 void tokenize (char* input, char** token, int* tp, char* t);
 void launch_program (char** token);
 void execute_commands (char** token, int* tp);
+void read_file();
+void redirecting_output(char** token, int* tp);
+void create_arrow_index (char** token, int length_args);
+
+char prompt [MAX_PROMPT];
+char direct [10][10];
+int arrows_ind [MAX_NUM_ARGS];
 
 int main(int argc, char *argv[]) {
 
 	//char userInput[MAX_INPUT_LINE];
 	char input[MAX_INPUT_LINE];
-
+	char *token [MAX_NUM_ARGS];
 	char *t;
 	//number of tokens (args)
 	int numTokens = 0;
@@ -36,33 +48,72 @@ int main(int argc, char *argv[]) {
 	
 	
 	//the shells repeating loop
-	cmd_loop(input, tp, t);
+	cmd_loop(input, token, tp, t);
 	//printf("/nnumTokens = &d" ,numTokens);
 	return 0;
 }
 	
-void cmd_loop(char* input, int* tp, char* t ) {
+void cmd_loop(char* input, char** token, int* tp, char* t ) {
 	
 	// For knowing the length of each user input
     int  line_len;
 	int loop = 0;
 	
+	read_file();
+	
 	// Repeating loop.
     while (!loop) {
-		char *token [MAX_NUM_ARGS];
-        fprintf(stdout, "> ");
+		
+		// display prompt.
+        fprintf(stdout, "%s ", prompt);
         fflush(stdout);
         fgets(input, MAX_INPUT_LINE, stdin);
+		// change new line to null.
         if (input[strlen(input) - 1] == '\n') {
             input[strlen(input) - 1] = '\0';
         }
 		
+		// calls fucntion to tokenize the input.
 		tokenize(input, token, tp, t);
+		
+		// execute built in commands and others.
 		execute_commands(token, tp);
 
 		int num_tokens = *tp;
+		if(strcmp(input, "exit") == 0) {
+            loop = 1;
+			break;
+			exit(0);
+        }
     } 
 }
+
+void read_file () {
+	
+	FILE *file_pointer;
+	if((file_pointer = fopen(".sh360rc", "r")) == 0) {
+		fprintf (stderr, "File not found.");
+	}
+	
+	fgets(prompt, MAX_PROMPT, file_pointer);
+	if( prompt[strlen(prompt) - 1] == '\n') {
+		prompt[strlen(prompt) - 1] = '\0';
+	}
+	
+	int i = 0;
+	
+	while (fgets(direct[i], sizeof(direct[i]), file_pointer) && i < 10) {
+		// Use a char pointer to go through the 2D array
+		char* dirP;
+		dirP = direct[i];
+		
+		if( dirP[strlen(dirP) - 1] == '\n') {
+			dirP[strlen(dirP) - 1] = '\0'; 
+		}
+		i++;
+	}
+}
+	
 	
 void tokenize (char* input, char** token, int* tp, char* t ) {
 	
@@ -101,15 +152,18 @@ void launch_program (char** token){
 	
 	// child process
 	if(pid == 0) {
-		printf("child: about to start...\n");
+		//printf("child: about to start...\n");
+		//printf("%s\n", token[0]);
 		execve(token[0], token, envp);
 		fprintf(stderr, "Command not recognized.\n");
+		
 		
 	} else if (pid < 0) {
 		fprintf(stderr, "Error forking.\n");
 		
 	} 
 	waitpid(pid, &status, 0);
+	
 		
 }
 
@@ -119,7 +173,10 @@ void execute_commands (char** token, int* tp) {
 	
 	if ( length_args == 0) {
 		printf("No arg entered\n");
-	} 
+	} else if (strcmp(token[0], "OR") == 0) {
+		redirecting_output(token, tp);
+	}
+	
 	// Built-in commands:
 	// for changeing directories (cd)
 	else if ( strcmp(token[0], "cd") == 0) {
@@ -137,10 +194,68 @@ void execute_commands (char** token, int* tp) {
 		
 	}
 	// Exit program
-	else if (!strcmp(token[0], "exit")) {
-            exit(0);
+	else if (strcmp(token[0], "exit") == 0) {
+		//exit(0);
 	}else {
 		launch_program(token);
+	}
+}
+
+void redirecting_output (char** token, int* tp) {
+	
+	int length_args = *tp;
+	char* envp[] = {0};
+	int pid, fd;
+	int status;
+	
+	// call function to find arrows
+	create_arrow_index(token, length_args);
+	
+	
+	// store the args exluding arrow
+	char* or_args [MAX_NUM_ARGS];
+	int j;
+	for ( j = 1; j < arrows_ind[0]; j++) {
+		or_args [j - 1] = token[j]; 
+	}
+	// store file name
+	char* output_file = token[arrows_ind[0] +1];
+	// Referenece: Addapted code from appendix_c
+	// write to file
+	if ((pid = fork()) == 0) {
+		fd = open(output_file, O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
+		if (fd == -1) {
+			fprintf(stderr, "cannot open %s for writting\n", output_file);
+			exit(1);
+		}
+		// instead of printing to stdout and stderr, print to fd
+		dup2(fd, 1);
+		dup2(fd, 2);
+		execve(or_args[0], or_args, envp);
+		fprintf(stderr, "OR: failed to execute proccess/n");
+	}
+	// wait for child
+	waitpid(pid, &status, 0);
+}
+
+void create_arrow_index (char** token, int length_args){
+	
+	// holds indexs of arrows
+	
+	int arrows_index = 0;
+	int i;
+	// Stores the indexs of where the arrows are.
+	
+	for (i = 0; i < length_args; i++) {
+			
+		if (strcmp(token[i], "->") == 0) {
+			arrows_ind[arrows_index] = i;
+			arrows_index++;
+		}
+	}
+	
+	if (arrows_index == 0) {
+		fprintf (stderr, "\'->\' not found.\n");
 	}
 }
 	
